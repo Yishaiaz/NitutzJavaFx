8e7d4beaa389f16be57a5bb59916c146764f122e
tamiryaffe <tamiryaffe144@gmail.com>
2018-10-21 23:46:01 +0300

    added functions: login and logout

--- src/Model/IModel.java
+++ src/Model/IModel.java
@@ -6,6 +6,18 @@ public interface IModel {
     boolean CreateAccount(String username, String password, String birthdate, String fName, String lName, String city);
     User SearchUser(String username);
     void UpdateAccount(String username, String password, String birthdate, String fName, String lName, String city);
-    User LogIn(String username, String password);
     String getLoggedUser();
+
+    /**
+     * login the input user by the username and password
+     * @param username- the user name to login.
+     * @param password- the user password to loin.
+     * @return the user name if the user name and password are valid, otherwise returns null.
+     */
+    String LogIn(String username, String password);
+
+    /**
+     * logout the current user in the system.
+     */
+    void logOut();
 }
