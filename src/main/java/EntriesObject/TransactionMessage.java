807d4e9cd481d9de16694f71b3519cd73e34b0a8
yishaia zabary <yz@sky-pick.com>
2018-12-08 18:09:44 +0200

    fixed messages objects, added to flight object the isbuyer field

--- src/main/java/EntriesObject/TransactionMessage.java
+++ src/main/java/EntriesObject/TransactionMessage.java
@@ -3,7 +3,7 @@ package EntriesObject;
 import java.util.Date;
 
 public class TransactionMessage extends AMessage {
-    public TransactionMessage(String message_id,  String user_owner_id, String title, String message_content, Date message_date, String from_user_id, String flight_id) {
+    public TransactionMessage(String message_id,  String user_owner_id, String title, String message_content, Date message_date, String from_user_id, String flight_id, boolean is_buyer) {
         this.message_id=message_id;
         this.user_owner_id =user_owner_id;
         this.title=title;
@@ -12,18 +12,6 @@ public class TransactionMessage extends AMessage {
         this.from_user_id =from_user_id ;
         this.is_transaction = true;
         this.flight_id =flight_id;
-    }
-
-    @Override
-    public String[] getAllData() {
-        String[] ans = new String[getColumnsTitles().length];
-        ans[0]= message_id;
-        ans[1]= user_owner_id;
-        ans[3]= title;
-        ans[4]= message_content;
-        ans[2]= message_date.toString();
-        ans[5]= from_user_id;
-        ans[6] = flight_id;
-        return ans;
+        this.is_buyer= is_buyer;
     }
 }
