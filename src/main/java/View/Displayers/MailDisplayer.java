207e54ac0b264d5501796efb1cc5398902c7c7e7
Dan <dan.avraham23@gmail.com>
2018-12-09 18:28:20 +0200

    MailDisplayer - displays the messageEntry in the mailbox MailBoxDisplayer - displays the mailbox

---
+++ src/main/java/View/Displayers/MailDisplayer.java
@@ -0,0 +1,73 @@
+package View.Displayers;
+
+import EntriesObject.AMessage;
+import EntriesObject.TransactionMessage;
+import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+
+import java.util.List;
+
+public class MailDisplayer extends GridPane {
+    private AMessage msg; // the message
+    private Label lbl_fromUser; //user that sent
+    private Label lbl_title; //message's title
+    private Label lbl_date; //message's date
+    private CheckBox cb_isTransaction; //is transaction message
+
+    public MailDisplayer(AMessage msg) {
+        setMessage(msg);
+        getColumnConstraints().add(new ColumnConstraints(75));
+        getColumnConstraints().add(new ColumnConstraints(100));
+        getColumnConstraints().add(new ColumnConstraints(100));
+        getColumnConstraints().add(new ColumnConstraints(100));
+    }
+
+    public static MailDisplayer getHeaders(){
+        MailDisplayer ans = new MailDisplayer(null);
+        ans.lbl_fromUser = new Label("From");
+        ans.lbl_title = new Label("Title");
+        ans.lbl_date = new Label("Date");
+        Label lbl_isTransaction = new Label("Is Transaction");
+        ans.add(ans.lbl_fromUser, 0, 0);
+        ans.add(ans.lbl_title, 1, 0);
+        ans.add(ans.lbl_date, 2, 0);
+        ans.add(lbl_isTransaction, 3, 0);
+        return ans;
+    }
+
+    /**
+     * sets the message to this displayer and initialize all fields
+     * @param message - the message to display
+     */
+    public void setMessage(AMessage message){
+        if(message == null)
+            return;
+        msg = message;
+        init();
+        add(lbl_fromUser, 0, 0);
+        add(lbl_title, 1, 0);
+        add(lbl_date, 2, 0);
+        add(cb_isTransaction, 3, 0);
+    }
+
+    /**
+     * initialize all the fields
+     */
+    private void init(){
+        String[] msgData = msg.getAllData();
+        lbl_fromUser = new Label(msgData[1]);
+        lbl_title = new Label(msgData[2]);
+        lbl_date = new Label(msgData[5]);
+        cb_isTransaction = new CheckBox("");
+        if(msg instanceof TransactionMessage){
+            cb_isTransaction.setSelected(true);
+        }
+        else{
+            cb_isTransaction.setSelected(false);
+        }
+        cb_isTransaction.setDisable(true);
+    }
+}
