2825a4534d396b463e3b5428a8d9a6ee553eb919
tamiryaffe <tamiryaffe144@gmail.com>
2018-12-11 17:54:20 +0200

    changed

--- src/main/java/Transaction/TransactionsEntry.java
+++ src/main/java/Transaction/TransactionsEntry.java
@@ -1,17 +1,18 @@
 package Transaction;
 
 import DataBaseConnection.IdbConnection;
-import DataBaseConnection.SqliteDbConnection;
 import EntriesObject.AEntry;
+import Flight.FlightEntry;
 import User.MailBox.Message;
 
-import java.util.ArrayList;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * available statuses:
- * Offer Received, Offer Approved, Closed, Rejected
+ * For Sale, Offer Received, Offer Approved, Closed, Rejected
  *
  */
 public class TransactionsEntry extends AEntry {
@@ -23,6 +24,8 @@ public class TransactionsEntry extends AEntry {
     private String flight_id;
     private String number_of_tickets;
     private String amountPayed;
+
+    private List<String> statuses;
     private IdbConnection idbConnection;
     /**
      * empty constructor mainly for testing
@@ -31,75 +34,124 @@ public class TransactionsEntry extends AEntry {
 
     /**
      *
-     * @param transaction_number
      * @param date
      * @param sellerUser_id
      * @param buyerUser_id
-     * @param transaction_status
      * @param flight_id
      * @param number_of_tickets
      * @param amountPayed
      * @throws Exception
      */
-    public TransactionsEntry(String transaction_number,Date date,String sellerUser_id,String buyerUser_id,String transaction_status,
-                             String flight_id,String number_of_tickets,String amountPayed,IdbConnection idbConnection) throws Exception{
-        String[] statuses={"Offer Received","Offer Approved","Closed","Rejected"};
-        ArrayList<String> possible_statuses= new ArrayList<String>();
-        for(String s:statuses){
-            possible_statuses.add(s);
+    public TransactionsEntry(Date date,String sellerUser_id,String buyerUser_id,
+                             String flight_id,String number_of_tickets,String amountPayed,IdbConnection idbConnection){
+        String[] initStatuses={"Offer Received","Offer Approved","Closed","Rejected"};
+        statuses= new ArrayList<>();
+        for(String s:initStatuses){
+            statuses.add(s);
         }
-        if(!possible_statuses.contains(transaction_status)){
-            throw new Exception("status not valid");
-        }
-        this.transaction_number=transaction_number;
+        int nextID=this.getMaximumId(getAllTransactions(idbConnection));
+        this.transaction_number=String.valueOf(nextID);
         this.sellerUser_id=sellerUser_id;
         this.buyerUser_id=buyerUser_id;
         this.number_of_tickets=number_of_tickets;
         this.amountPayed=amountPayed;
         this.flight_id=flight_id;
         this.transaction_date =date;
-        this.transaction_status=transaction_status;
         this.idbConnection=idbConnection;
-        sendOfferRecivedMsg();
+        this.transaction_status="";
     }
 
-    public TransactionsEntry(TransactionsEntry other,IdbConnection idbConnection){
-        String[] a = other.getAllData();
-        this.transaction_number = a[0];
-        this.sellerUser_id=a[1];
-        this.buyerUser_id=a[2];
-        this.number_of_tickets=a[3];
-        this.amountPayed=a[4];
-        this.flight_id=a[5];
-        this.transaction_date = Date.valueOf(a[6]);
-        this.transaction_status=a[7];
-        this.idbConnection= idbConnection;
+    public void setTransaction_status(String transaction_status) throws Exception {
+        if(!statuses.contains(transaction_status)){
+            throw new Exception("status not valid");
+        }
+        this.transaction_status=transaction_status;
+        if(transaction_status.equals("Offer Received")){
+            sendOfferRecivedMsg();
+        }
+        if(transaction_status.equals("Offer Approved")){
+            sendOfferApprovedMsg();
+        }
+        if(transaction_status.equals("Closed")){
+            sendOfferFinishedMsg();
+        }
     }
 
     private void sendOfferRecivedMsg() {
-        //String message_id,  String user_owner_id, String title,String buyerUser_id, Date message_date, String from_user_id, String transaction_id) {
-        //need to change msg. to not have id.
-
-        String msgContent="You got an offer from userName about your postDetails";
-        Message message=new Message("-1",sellerUser_id,"Offer Recived",msgContent,new Date(System.currentTimeMillis()),"-1",transaction_number);
+        String flightDetails = getFlightDetails();
+        String msgContent="You got an offer from the user: "+buyerUser_id+" about your flight post: "
+                            +flightDetails+System.getProperty("line.separator")+"confirm or denied.";
+        Message message=new Message("-1",sellerUser_id,"Offer Received",msgContent,new Date(System.currentTimeMillis()),"-1",transaction_number);
         sendMessage(sellerUser_id,message);
     }
 
     private void sendOfferApprovedMsg() {
-        //String message_id,  String user_owner_id, String title,String buyerUser_id, Date message_date, String from_user_id, String transaction_id) {
-        //need to change msg.
-        String msgContent="";
-        Message message=new Message("-1",sellerUser_id,"Offer Recived",msgContent,new Date(System.currentTimeMillis()),"-1",transaction_number);
+        String flightDetails = getFlightDetails();
+        String msgContent="user: "+sellerUser_id+" approved your offer about flight post: "+flightDetails
+                                    +System.getProperty("line.separator")+"to purchase press on payment link below.";
+        Message message=new Message("-1",buyerUser_id,"Offer Approved",msgContent,new Date(System.currentTimeMillis()),"-1",transaction_number);
         sendMessage(buyerUser_id,message);
+    }
+
+    private void sendOfferFinishedMsg() {
+        String flightDetails = getFlightDetails();
+        String msgContent="Transaction of flight post: "+flightDetails+" has been complete and payment has been payed"
+                +System.getProperty("line.separator")+"thank you for using Vacation4U.";
+        Message messageToSeller=new Message("-1",sellerUser_id,"Offer Closed",msgContent,new Date(System.currentTimeMillis()),"-1",transaction_number);
+        Message messageToBuyer=new Message("-1",buyerUser_id,"Offer Closed",msgContent,new Date(System.currentTimeMillis()),"-1",transaction_number);
+        sendMessage(sellerUser_id,messageToSeller);
+        sendMessage(buyerUser_id,messageToBuyer);
+    }
+
+    private String getFlightDetails() {
+        String[] flightEntry = null;
+
+        try {
+            flightEntry = idbConnection.getEntryById(flight_id, new FlightEntry(""));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "departure at " + flightEntry[3] + " from " + flightEntry[7] + " to " + flightEntry[12] + " at the price of " + flightEntry[10];
+    }
+
+    private void updateMsgId(Message message,String receiverUserID){
+        int max=0;
+        try{
+            LinkedList<String[]> all= idbConnection.getAllFromTable(new Message(receiverUserID));
+            if(all==null){
+                message.setMessage_id(String.valueOf(max+1));
+                return;
+            }
+            for(String s[]: all){
+                if(Integer.valueOf(s[0]) > max) max = Integer.valueOf(s[0]);
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        message.setMessage_id(String.valueOf(max+1));
     }
 
     private void sendMessage(String receiverUserID, Message message){
         try{
+            updateMsgId(message,receiverUserID);
             idbConnection.insertToDbByTableName(receiverUserID+"_mailbox", message);
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
     }
+
+    private int getMaximumId(LinkedList<String[]> all){
+        int max=0;
+        if (all==null || all.size()==0){
+            return 1;
+        }
+        for(String[] s: all){
+            if( Integer.valueOf(s[0]) > max) max =Integer.valueOf(s[0]);
+        }
+        return max+1;
+    }
+
+    //<editor-fold desc="DB">
     @Override
     public boolean validateEntry() {
         return true;
@@ -156,32 +208,13 @@ public class TransactionsEntry extends AEntry {
         idbConnection.deleteById(this);
     }
 
-    public void updateTransactionStatus(String newStatus) throws Exception{
-        String[] statuses={"Offer Received","Offer Approved","Closed","Rejected"};
-        ArrayList<String> possible_statuses= new ArrayList<String>();
-        for(String s:statuses){
-            possible_statuses.add(s);
-        }
-        if(!possible_statuses.contains(newStatus)){
-            throw new Exception("status not valid");
-        }
-        String[] ans=getAllData();
-        ans[4] = newStatus;
-        idbConnection.updateEntry(this, ans);
-    }
-
-    public LinkedList<TransactionsEntry> getAllTransactionsByUserId(IdbConnection idbConnection){
-        try{
-            LinkedList<String[]> all=idbConnection.getAllFromTable(this);
-            LinkedList<TransactionsEntry> transactionsEntries = new LinkedList<>();
-            for(String[] r:all){
-                transactionsEntries.add(new TransactionsEntry(r[0],Date.valueOf(r[1]),r[2],r[3],r[4],r[5],r[6],r[7],idbConnection));
-            }
-            return transactionsEntries;
-        }catch(Exception e){
+    public LinkedList<String[]> getAllTransactions(IdbConnection idbConnection) {
+        try {
+            return idbConnection.getAllFromTable(this);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return null;
-
     }
+    //</editor-fold>
 }
